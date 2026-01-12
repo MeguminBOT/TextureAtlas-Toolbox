eb3011fab79290ad60d16b8b88e88c6a8ae96a1e
AutisticLulu <discord.meguminbot@gmail.com>
2026-01-12 15:29:50 +0100

    Bump version to 2.0.2

--- src/utils/version.py
+++ src/utils/version.py
@@ -9,7 +9,7 @@ from __future__ import annotations
 import re
 
 APP_NAME = "TextureAtlas Toolbox"
-APP_VERSION = "2.0.1"
+APP_VERSION = "2.0.2"
 REPO_OWNER = "MeguminBOT"
 REPO_NAME = "TextureAtlas-Toolbox"
 
